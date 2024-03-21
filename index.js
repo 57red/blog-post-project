2c7c4ca533681c05d7c3833ed646068bda79cd73
frede <frederickgetueza@gmail.com>
2024-03-21 21:50:29 +0800

    Initialize API and backend server

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`API is running at http://localhost:${port}.`);
+});
